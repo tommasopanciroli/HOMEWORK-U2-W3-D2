7eeeb050cad8db1bb7b19f50efcbeac7500bbdd8
tommaso.panciroli <tommypanci@gmail.com>
2024-11-12 16:00:30 +0100

    update js

---
+++ ex1.js
@@ -0,0 +1,37 @@
+const key = 'memory'
+
+const form = document.querySelector('form')
+
+const saveButton = document.getElementById('save')
+const removeButton = document.getElementById('remove')
+
+const text = document.getElementById('inputName').value
+
+const pDiPorcoDio = document.querySelector('p')
+
+let memoryArr
+if (localStorage.getItem(key)) {
+  memoryArr = JSON.parse(localStorage.getItem(key))
+} else {
+  memoryArr = []
+}
+
+if (memoryArr.length > 0) {
+  let name = memoryArr.length[-1]
+  pDiPorcoDio.innerHTML = `${memoryArr[name]}`
+}
+
+function buttonSave() {
+  if (text.length > 1) {
+    memoryArr.push(text)
+    localStorage.setItem(key, JSON.stringify(memoryArr))
+    let name = memoryArr.length[-1]
+    p.innerHTML = `${memoryArr[name]}`
+    form.reset()
+  }
+}
+
+form.addEventListener('submit', function (e) {
+  buttonSave()
+  e.preventDefault()
+})
